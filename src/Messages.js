f883f69ae8b900c092605c4b5433d2679e38d512
Ivan Raštegorac <ivan.rastegorac@gmail.com>
2021-12-03 09:46:05 +0100

    Edited Input and Messages

--- src/Messages.js
+++ src/Messages.js
@@ -18,7 +18,7 @@ class Messages extends Component {
     const className = messageFromMe ?
       "Messages-message currentMember" : "Messages-message";
     return (
-      <li className={className}>
+      <li key={Math.floor(Math.random() * 10000)} className={className}>
         <span
         className="avatar"
         style={{backgroundColor: member.clientData.color}}
